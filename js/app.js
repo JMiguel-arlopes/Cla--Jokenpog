f717cb5c06e7f879ba03e5100494b6d695e55c1a
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-08 15:37:29 -0300

    inserindo manipulação basica do index

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const btnMenu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+btnMenu.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+})
+
+
+window.addEventListener("scroll", () => {
+    navbar.classList.remove('active');
+})
