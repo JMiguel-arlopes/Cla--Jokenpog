635e122e8d9446832734e63edbe1093683a73735
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-09 08:34:07 -0300

    testando código com sistema de interação inédito

--- js/app.js
+++ js/app.js
@@ -9,3 +9,53 @@ btnMenu.addEventListener('click', () => {
 window.addEventListener("scroll", () => {
     navbar.classList.remove('active');
 })
+
+let cod = document.querySelector('.cod');
+let mlbb = document.querySelector('.mlbb');
+let eco = document.querySelector('.eco');
+
+mlbb.addEventListener('click', () => {
+    cod.classList.toggle('activeMlbb');
+    mlbb.classList.toggle('activeMlbb');
+    eco.classList.toggle('activeMlbb')
+
+    cod.classList.remove('activeCod');
+    mlbb.classList.remove('activeCod');
+    eco.classList.remove('activeCod');
+
+    cod.classList.remove('activeEco');
+    mlbb.classList.remove('activeEco');
+    eco.classList.remove('activeEco');
+})
+
+cod.addEventListener('click', () => {
+    cod.classList.toggle('activeCod');
+    mlbb.classList.toggle('activeCod');
+    eco.classList.toggle('activeCod')
+
+    cod.classList.remove('activeMlbb');
+    mlbb.classList.remove('activeMlbb');
+    eco.classList.remove('activeMlbb');
+
+    cod.classList.remove('activeEco');
+    mlbb.classList.remove('activeEco');
+    eco.classList.remove('activeEco');
+})
+
+eco.addEventListener('click', () => {
+    cod.classList.toggle('activeEco');
+    mlbb.classList.toggle('activeEco');
+    eco.classList.toggle('activeEco');
+
+    // if(eco.classList.contains(activeEco)) {
+    //     eco.style.color='black';
+    // }
+
+    cod.classList.remove('activeMlbb');
+    mlbb.classList.remove('activeMlbb');
+    eco.classList.remove('activeMlbb');
+
+    cod.classList.remove('activeCod');
+    mlbb.classList.remove('activeCod');
+    eco.classList.remove('activeCod');
+})
