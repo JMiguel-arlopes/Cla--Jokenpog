ad4490c2e59cd25668d17753a7a65ba1c44e593d
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-11 20:14:34 -0300

    inserindo interação ao tocar no user

--- js/app.js
+++ js/app.js
@@ -10,3 +10,20 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove('active');
 })
 
+
+
+
+
+const containerPerfil = document.querySelectorAll('.perfil');
+const contentPerfil = document.querySelectorAll('.content-review');
+const UserPerfil = document.querySelectorAll('.user');
+
+containerPerfil.forEach((perfil, i) => {
+    perfil.addEventListener('click', () => {
+        UserPerfil.forEach((foto, j) => {
+            if(i == j) {
+                foto.classList.toggle('active');
+            }  
+        })
+    })
+})
