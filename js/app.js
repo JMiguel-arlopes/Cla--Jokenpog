9bfb48920a39e410eb2093678970c7fb5b3c690e
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-14 04:51:51 -0300

    inserindo manipulação as plataformas

--- js/app.js
+++ js/app.js
@@ -16,26 +16,112 @@ const painelInvestimentos = document.querySelector('.box');
 const painelCOD = document.querySelector('.box-2');
 const painelMLBB = document.querySelector('.box-3');
 
+const titulo = document.querySelectorAll('.titulo-plataforma');
+const link = document.querySelectorAll('.link-plataforma');
+
+
 
 painelInvestimentos.addEventListener('click', () => {
+    painelInvestimentos.classList.remove('disabled');
     painelInvestimentos.classList.add('active');
-    
+
     painelCOD.classList.remove('active');
     painelMLBB.classList.remove('active');
+
+    painelCOD.classList.add('disabled');
+    painelMLBB.classList.add('disabled');
+
+
+    link.forEach((linka, i) => {
+
+        if(i === 0) {
+            linka.classList.remove('hide');
+
+            titulo.forEach((title, j) => {
+                if(j === i) {
+                    title.classList.add('hide');
+                }
+            }) 
+        } else {
+            if (!linka.classList.contains('hide'))
+                linka.classList.add('hide');
+
+                titulo.forEach((title, j) => {
+                    if(j === i) {
+                        title.classList.remove('hide');
+                    }
+                }) 
+        }
+    })
 })
 
+
 painelCOD.addEventListener('click', () => {
+    painelCOD.classList.remove('disabled');
     painelCOD.classList.add('active');
 
     painelInvestimentos.classList.remove('active');
     painelMLBB.classList.remove('active');
+
+    painelInvestimentos.classList.add('disabled');
+    painelMLBB.classList.add('disabled');
+
+
+    link.forEach((linka, i) => {
+
+        if(i === 1) {
+            linka.classList.remove('hide');
+
+            titulo.forEach((title, j) => {
+                if(j === i) {
+                    title.classList.add('hide');
+                }
+            })
+        } else {
+            if (!linka.classList.contains('hide'))
+                linka.classList.add('hide');
+
+                titulo.forEach((title, j) => {
+                    if(j === i) {
+                        title.classList.remove('hide');
+                    }
+                }) 
+        }
+    })
 })
 
+
 painelMLBB.addEventListener('click', () => {
+    painelMLBB.classList.remove('disabled');
     painelMLBB.classList.add('active');
 
     painelInvestimentos.classList.remove('active');
     painelCOD.classList.remove('active');
+
+    painelInvestimentos.classList.add('disabled');
+    painelCOD.classList.add('disabled');
+
+    link.forEach((linka, i) => {
+
+        if(i === 2) {
+            linka.classList.remove('hide');
+
+            titulo.forEach((title, j) => {
+                if(j === i) {
+                    title.classList.add('hide');
+                }
+            })
+        } else {
+            if (!linka.classList.contains('hide'))
+                linka.classList.add('hide');
+
+                titulo.forEach((title, j) => {
+                    if(j === i) {
+                        title.classList.remove('hide');
+                    }
+                }) 
+        }
+    })
 })
 
 
