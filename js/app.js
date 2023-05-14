d405c423bc9cb6e17faf5f0c9a2be32606ac0cc6
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-13 23:54:58 -0300

    inserindo interação na plataforma

--- js/app.js
+++ js/app.js
@@ -10,6 +10,41 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove('active');
 })
 
+// plataformas:
+
+const painelInvestimentos = document.querySelector('.box');
+const painelCOD = document.querySelector('.box-2');
+const painelMLBB = document.querySelector('.box-3');
+
+
+painelInvestimentos.addEventListener('click', () => {
+    painelInvestimentos.classList.add('active');
+    
+    painelCOD.classList.remove('active');
+    painelMLBB.classList.remove('active');
+})
+
+painelCOD.addEventListener('click', () => {
+    painelCOD.classList.add('active');
+
+    painelInvestimentos.classList.remove('active');
+    painelMLBB.classList.remove('active');
+})
+
+painelMLBB.addEventListener('click', () => {
+    painelMLBB.classList.add('active');
+
+    painelInvestimentos.classList.remove('active');
+    painelCOD.classList.remove('active');
+})
+
+
+
+
+
+
+
+
 
 
 
@@ -27,3 +62,5 @@ containerPerfil.forEach((perfil, i) => {
         })
     })
 })
+
+
