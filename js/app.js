d2ea7c9112b2da35ff4c47345098dcce46d632e0
JM-arlopes <jmiguelarlopes@gmail.com>
2023-05-15 22:50:00 -0300

    deixando o código mais genérico

--- js/app.js
+++ js/app.js
@@ -15,114 +15,193 @@ window.addEventListener("scroll", () => {
 const painelInvestimentos = document.querySelector('.box');
 const painelCOD = document.querySelector('.box-2');
 const painelMLBB = document.querySelector('.box-3');
+const caixas = document.querySelectorAll('[data-caixa]');
 
 const titulo = document.querySelectorAll('.titulo-plataforma');
 const link = document.querySelectorAll('.link-plataforma');
 
 
 
-painelInvestimentos.addEventListener('click', () => {
-    painelInvestimentos.classList.remove('disabled');
-    painelInvestimentos.classList.add('active');
 
-    painelCOD.classList.remove('active');
-    painelMLBB.classList.remove('active');
+caixas.forEach(caixa => {
+    caixa.addEventListener('click', (evento) => {
+        const data = evento.target.dataset.caixa
+        console.log(data)
 
-    painelCOD.classList.add('disabled');
-    painelMLBB.classList.add('disabled');
+        if (data === "mlbb") {
 
+            painelMLBB.classList.remove('disabled');
+            painelMLBB.classList.add('active');
 
-    link.forEach((linka, i) => {
+            painelInvestimentos.classList.remove('active');
+            painelCOD.classList.remove('active');
 
-        if(i === 0) {
-            linka.classList.remove('hide');
+            painelInvestimentos.classList.add('disabled');
+            painelCOD.classList.add('disabled');
+        }
 
-            titulo.forEach((title, j) => {
-                if(j === i) {
-                    title.classList.add('hide');
-                }
-            }) 
-        } else {
-            if (!linka.classList.contains('hide'))
-                linka.classList.add('hide');
+        if (data === "eco") {
+            painelInvestimentos.classList.remove('disabled');
+            painelInvestimentos.classList.add('active');
 
+            painelCOD.classList.remove('active');
+            painelMLBB.classList.remove('active');
+
+            painelCOD.classList.add('disabled');
+            painelMLBB.classList.add('disabled');
+        }
+
+        if (data === "cod") {
+            painelCOD.classList.remove('disabled');
+            painelCOD.classList.add('active');
+
+            painelInvestimentos.classList.remove('active');
+            painelMLBB.classList.remove('active');
+
+            painelInvestimentos.classList.add('disabled');
+            painelMLBB.classList.add('disabled');
+
+            
+        }
+
+        const linkEsp = evento.target.querySelector('[data-link]')
+
+        console.log(linkEsp);
+
+        linkEsp.classList.remove('hide');
+
+        link.forEach((linka, i) => {
+            if(i === 0) {
+                linka.classList.remove('hide');
+    
                 titulo.forEach((title, j) => {
                     if(j === i) {
-                        title.classList.remove('hide');
+                        title.classList.add('hide');
                     }
                 }) 
-        }
+            } else {
+                if (!linka.classList.contains('hide'))
+                    linka.classList.add('hide');
+    
+                    titulo.forEach((title, j) => {
+                        if(j === i) {
+                            title.classList.remove('hide');
+                        }
+                    }) 
+            }
+        })
     })
 })
 
 
-painelCOD.addEventListener('click', () => {
-    painelCOD.classList.remove('disabled');
-    painelCOD.classList.add('active');
-
-    painelInvestimentos.classList.remove('active');
-    painelMLBB.classList.remove('active');
-
-    painelInvestimentos.classList.add('disabled');
-    painelMLBB.classList.add('disabled');
 
 
-    link.forEach((linka, i) => {
-
-        if(i === 1) {
-            linka.classList.remove('hide');
-
-            titulo.forEach((title, j) => {
-                if(j === i) {
-                    title.classList.add('hide');
-                }
-            })
-        } else {
-            if (!linka.classList.contains('hide'))
-                linka.classList.add('hide');
-
-                titulo.forEach((title, j) => {
-                    if(j === i) {
-                        title.classList.remove('hide');
-                    }
-                }) 
-        }
-    })
-})
 
 
-painelMLBB.addEventListener('click', () => {
-    painelMLBB.classList.remove('disabled');
-    painelMLBB.classList.add('active');
 
-    painelInvestimentos.classList.remove('active');
-    painelCOD.classList.remove('active');
 
-    painelInvestimentos.classList.add('disabled');
-    painelCOD.classList.add('disabled');
+// painelInvestimentos.addEventListener('click', () => {
+//     painelInvestimentos.classList.remove('disabled');
+//     painelInvestimentos.classList.add('active');
 
-    link.forEach((linka, i) => {
+//     painelCOD.classList.remove('active');
+//     painelMLBB.classList.remove('active');
 
-        if(i === 2) {
-            linka.classList.remove('hide');
+//     painelCOD.classList.add('disabled');
+//     painelMLBB.classList.add('disabled');
 
-            titulo.forEach((title, j) => {
-                if(j === i) {
-                    title.classList.add('hide');
-                }
-            })
-        } else {
-            if (!linka.classList.contains('hide'))
-                linka.classList.add('hide');
 
-                titulo.forEach((title, j) => {
-                    if(j === i) {
-                        title.classList.remove('hide');
-                    }
-                }) 
-        }
-    })
-})
+//     link.forEach((linka, i) => {
+
+//         if(i === 0) {
+//             linka.classList.remove('hide');
+
+//             titulo.forEach((title, j) => {
+//                 if(j === i) {
+//                     title.classList.add('hide');
+//                 }
+//             }) 
+//         } else {
+//             if (!linka.classList.contains('hide'))
+//                 linka.classList.add('hide');
+
+//                 titulo.forEach((title, j) => {
+//                     if(j === i) {
+//                         title.classList.remove('hide');
+//                     }
+//                 }) 
+//         }
+//     })
+// })
+
+
+// painelCOD.addEventListener('click', () => {
+//     painelCOD.classList.remove('disabled');
+//     painelCOD.classList.add('active');
+
+//     painelInvestimentos.classList.remove('active');
+//     painelMLBB.classList.remove('active');
+
+//     painelInvestimentos.classList.add('disabled');
+//     painelMLBB.classList.add('disabled');
+
+
+//     link.forEach((linka, i) => {
+
+//         if(i === 1) {
+//             linka.classList.remove('hide');
+
+//             titulo.forEach((title, j) => {
+//                 if(j === i) {
+//                     title.classList.add('hide');
+//                 }
+//             })
+//         } else {
+//             if (!linka.classList.contains('hide'))
+//                 linka.classList.add('hide');
+
+//                 titulo.forEach((title, j) => {
+//                     if(j === i) {
+//                         title.classList.remove('hide');
+//                     }
+//                 }) 
+//         }
+//     })
+// })
+
+
+// painelMLBB.addEventListener('click', () => {
+//     painelMLBB.classList.remove('disabled');
+//     painelMLBB.classList.add('active');
+
+//     painelInvestimentos.classList.remove('active');
+//     painelCOD.classList.remove('active');
+
+//     painelInvestimentos.classList.add('disabled');
+//     painelCOD.classList.add('disabled');
+
+//     link.forEach((linka, i) => {
+
+//         if(i === 2) {
+//             linka.classList.remove('hide');
+
+//             titulo.forEach((title, j) => {
+//                 if(j === i) {
+//                     title.classList.add('hide');
+//                 }
+//             })
+//         } else {
+//             if (!linka.classList.contains('hide'))
+//                 linka.classList.add('hide');
+
+//                 titulo.forEach((title, j) => {
+//                     if(j === i) {
+//                         title.classList.remove('hide');
+//                     }
+//                 }) 
+//         }
+//     })
+// })
 
 
 
