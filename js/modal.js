f363b1beb985b0d6a38459e3827d941e1191b5bf
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-12 23:10:34 -0300

    inserindo escuta para o fim da animação da barra

--- js/modal.js
+++ js/modal.js
@@ -2,29 +2,45 @@ const containerModel = document.querySelector('[data-model]');
 const btnModel = document.querySelectorAll('.swipper .slides .slide span');
 const models = document.querySelectorAll('[data-modelSlide]');
 
+
+
+
 btnModel.forEach(btn => {
     btn.addEventListener('click', (el) => {
         
         models.forEach(model => {
             if(model.dataset.modelslide == el.target.dataset.btn) {
-                model.classList.add('active');
+                
 
+                const bannerPrincipal = model.querySelector('.modal-perfil');
+                const bannerGrafico = model.querySelector('.modal-grafico');
+                const btnGraficos = model.querySelector('[data-btnGraficos]')
+
+                model.classList.add('active');
                 models.forEach(i => {
                     if(i != model) {
                         i.classList.remove('active');
                     }
                 })
-            }
 
-            // const btnModalEstatisticas = model.querySelector('.name-modal')
+                // ao clique o botão, some layout 1 e entra os graficos.
+                
+                
 
-            
-            
-            // btnModalEstatisticas.addEventListener('click', () => {
-            //     model
-            // })
-            
-            
+                btnGraficos.addEventListener('click', () => {
+
+                    if(bannerPrincipal.classList.contains('active')) {
+                        
+                        bannerPrincipal.classList.replace('active', 'disabled');
+                        bannerGrafico.classList.replace('disabled', 'active');
+                    } else {
+                        bannerPrincipal.classList.replace('disabled', 'active');
+                        bannerGrafico.classList.replace('active', 'disabled');
+                    }
+                        
+
+                })
+            }            
         })
     
         containerModel.classList.add('active');
@@ -40,4 +56,11 @@ containerModel.addEventListener('click', el => {
 
 })
 
+const barraDeProgresso = document.querySelectorAll('.progress-bar');
 
+
+barraDeProgresso.forEach(el => {
+    el.addEventListener('animationend', () => {
+        el.querySelector('div').classList.add('brilho')
+    })
+})
