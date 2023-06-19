7e0386060ead36d136651b9c610c2b56298d338e
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-18 23:58:58 -0300

    deixando o código majoritariamente genérico e mais curto

--- js/regras.js
+++ js/regras.js
@@ -1,191 +1,23 @@
-const btnObjetivos = document.querySelector('.btn-objetivos');
-const btnProibicoes = document.querySelector('.btn-proibicoes');
-const btnDireitos = document.querySelector('.btn-direitos');
-const btnDeveres = document.querySelector('.btn-deveres');
+const btnsRegra = document.querySelectorAll('.title-regra');
+const listaRegras = document.querySelectorAll('.lista-regras'); 
+const imgsRegra = document.querySelectorAll('.img-regra'); 
 
-const objetivos = document.querySelector('.lista-objetivos');
-const proibicoes = document.querySelector('.lista-proibicoes');
-const direitos = document.querySelector('.lista-direitos');
-const deveres = document.querySelector('.lista-deveres');
-
-const imgValir = document.querySelector('.valir');
-const imgValirCollector = document.querySelector('.valir-collector');
-const imgKagura = document.querySelector('.kagura');
-const imgWanwan = document.querySelector('.wanwan');
-
-
-btnObjetivos.addEventListener('click', () => {
-
-    imgValir.classList.add('active');
-    imgKagura.classList.remove('active');
-    imgValirCollector.classList.remove('active');
-    imgWanwan.classList.remove('active');
-
-    btnObjetivos.classList.add('active');
-    btnProibicoes.classList.remove('active');
-    btnDireitos.classList.remove('active');
-    btnDeveres.classList.remove('active');
-
-
-    objetivos.classList.add('active');
-    proibicoes.classList.remove('active');
-    direitos.classList.remove('active');
-    deveres.classList.remove('active');
-})
-
-btnProibicoes.addEventListener('click', () => { 
-
-    imgValir.classList.remove('active');
-    imgKagura.classList.add('active');
-    imgValirCollector.classList.remove('active');
-    imgWanwan.classList.remove('active');
-
-    btnObjetivos.classList.remove('active');
-    btnProibicoes.classList.add('active');
-    btnDireitos.classList.remove('active');
-    btnDeveres.classList.remove('active');
-
-    objetivos.classList.remove('active');
-    proibicoes.classList.add('active');
-    direitos.classList.remove('active');
-    deveres.classList.remove('active');
-})
-
-
-btnDireitos.addEventListener('click', () => {
-
-    imgValir.classList.remove('active');
-    imgKagura.classList.remove('active');
-    imgValirCollector.classList.add('active');
-    imgWanwan.classList.remove('active');
-
-    btnObjetivos.classList.remove('active');
-    btnProibicoes.classList.remove('active');
-    btnDireitos.classList.add('active');
-    btnDeveres.classList.remove('active');
-
-    proibicoes.classList.remove('active');
-    objetivos.classList.remove('active');
-    direitos.classList.add('active');
-    deveres.classList.remove('active');
-})
-
-
-btnDeveres.addEventListener('click', () => {
-
-    imgValir.classList.remove('active');
-    imgKagura.classList.remove('active');
-    imgValirCollector.classList.remove('active');
-    imgWanwan.classList.add('active');
-
-
-    btnObjetivos.classList.remove('active');
-    btnProibicoes.classList.remove('active');
-    btnDireitos.classList.remove('active');
-    btnDeveres.classList.add('active');
-
-    proibicoes.classList.remove('active');
-    objetivos.classList.remove('active');
-    direitos.classList.remove('active');
-    deveres.classList.add('active');
-})
-
-
-
-
-
-
-
-
-
-
-
-// interação extensoes:
-
-const containerBoxs = document.querySelector('.box-extensoes');
-const caixas = document.querySelectorAll('[data-box]');
-
-// 1º forma:
-
-caixas.forEach(elemento => {
-    elemento.addEventListener('click', () => {
-        const dataElemento = elemento.dataset.box;
-
-        caixas.forEach(i => {
-            i.classList.remove('disabled');
-
-            if (i != elemento) {
-                
-                const noClickBox = i.dataset.box;
-                containerBoxs.classList.remove(`active-box-${noClickBox}`);
-                i.classList.add('disabled');
-            }
-        })
-        containerBoxs.classList.add(`active-box-${dataElemento}`);
-    })
-})
+function ativarRegras() {
+    const dataBtnRegra = this.dataset.regra;
     
-
-// 2º forma :
-
-// containerBoxs.addEventListener('click', (e) => {
-//     const caixa = e.target.parentNode.parentNode;
-//     const dataCaixa = caixa.dataset.box;
-
-//     if(caixa.dataset.box) {
-//         console.log('kk')
-//         caixas.forEach(box => {
-
-//             box.classList.remove('disabled');
-
-//             if(box != caixa) {
-//                 const dataBox = box.dataset.box;
-//                 containerBoxs.classList.remove(`active-box-${dataBox}`);
-//                 box.classList.add('disabled');
-//             }
-//         })
-//         containerBoxs.classList.add(`active-box-${dataCaixa}`)
-//     } else {
-//         caixas.forEach(box => {
-//             box.classList.remove('disabled')
-//             containerBoxs.classList.remove(`active-box-${dataCaixa}`)
-//         })
-//     }
-// })
-
-
-// window.addEventListener('scroll', () => {
-//     caixas.forEach(box => {
-//         const dataBox = box.dataset.box;
-//         containerBoxs.classList.remove(`active-box-${dataBox}`);
-//     })
-// })
-
-
-
-
-
-let counter = 1;
-const inputs = document.querySelectorAll('[data-input]');
-
-function slide() {
-    
-    inputs.forEach(btnInput => {
-        btnInput.addEventListener('click', el => {
-            const clickInput = el.target.dataset.input;
-            counter = clickInput;
-        })
+    listaRegras.forEach(regra => {
+        const dataRegra = regra.dataset.regra;
+        dataBtnRegra == dataRegra ? regra.classList.add('active') : regra.classList.remove('active');
     })
 
-    if (counter > inputs.length) {
-        counter = 1;
-    }
+    imgsRegra.forEach(imgRegra => {
+        const dataImg = imgRegra.dataset.regra;
+        dataBtnRegra == dataImg ? imgRegra.classList.add('active') : imgRegra.classList.remove('active');
+    })
 
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
 
-    setTimeout(slide, 10000)
+    btnsRegra.forEach(btn => btn.classList.remove('active'))
+    this.classList.add('active')
 }
 
-// slide();
-
+btnsRegra.forEach(btn => btn.addEventListener('click', ativarRegras))
