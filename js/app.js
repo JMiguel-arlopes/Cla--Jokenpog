e279653692a7e788d623f4126e3e0beee99d546a
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-19 23:51:43 -0300

    deixando o código genérico e mais sintatico

--- js/app.js
+++ js/app.js
@@ -12,103 +12,21 @@ window.addEventListener("scroll", () => {
 
 // plataformas:
 
-const painelInvestimentos = document.querySelector('.box');
-const painelCOD = document.querySelector('.box-2');
-const painelMLBB = document.querySelector('.box-3');
 const caixas = document.querySelectorAll('[data-caixa]');
 
-const titulo = document.querySelectorAll('.titulo-plataforma');
-const link = document.querySelectorAll('.link-plataforma');
-
-caixas.forEach(caixa => {
-    caixa.addEventListener('click', (evento) => {
-        const dataGame = evento.target.dataset.caixa;
-        const game = evento.target;
-        
-
-        if (dataGame === "mlbb") {
-            if(game.classList.contains('disabled')) {
-
-                game.classList.replace('disabled', 'active');
-                painelInvestimentos.classList.replace('active', 'disabled');
-                painelCOD.classList.replace('active', 'disabled');
-            } else {
-
-                game.classList.add('active');
-                painelInvestimentos.classList.add('disabled');
-                painelCOD.classList.add('disabled');
-            }
-        }
-
-        if (dataGame === "eco") {
-            if(game.classList.contains('disabled')) {
-
-                game.classList.replace('disabled', 'active');
-                painelMLBB.classList.replace('active', 'disabled');
-                painelCOD.classList.replace('active', 'disabled');
-            } else {
-
-                game.classList.add('active');
-                painelMLBB.classList.add('disabled');
-                painelCOD.classList.add('disabled');
-            }
-        }
-
-        if (dataGame === "cod") {
-            if(game.classList.contains('disabled')) {
-
-                game.classList.replace('disabled', 'active');
-                painelInvestimentos.classList.replace('active', 'disabled');
-                painelMLBB.classList.replace('active', 'disabled');
-            } else {
-
-                game.classList.add('active');
-                painelInvestimentos.classList.add('disabled');
-                painelMLBB.classList.add('disabled');
-            }
-        }
-
-        link.forEach((linka) => {
-            const l = linka.dataset.link;
-
-            if(l == dataGame) {
-                linka.classList.remove('hide');
-                game.querySelector('[data-titulo]').classList.add('hide');
-            } 
-
-            if(l != dataGame) {
-                linka.classList.add('hide');
-
-                titulo.forEach((title) => {
-                    const t = title.dataset.titulo;
-                    if(t != dataGame) {
-                        title.classList.remove('hide')
-                    }  
-                })
-            }
-
-        })
+function ativarCaixa() {
+    const dataBoxHit = this.dataset.caixa;
+    caixas.forEach(el => {
+        el.classList.contains('active') ? el.classList.replace('active', 'disabled') : el.classList.add('disabled');
+        if(dataBoxHit != el.dataset.caixa) return;
+        el.classList.contains('disabled') ? el.classList.replace('disabled', 'active') : el.classList.add('active');
     })
-})
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+caixas.forEach(box => box.addEventListener('click', ativarCaixa)) // TROCA POR THIS
 
 
 const containerPerfil = document.querySelectorAll('.perfil');
-const contentPerfil = document.querySelectorAll('.content-review');
 const UserPerfil = document.querySelectorAll('.user');
 
 containerPerfil.forEach((perfil, i) => {
