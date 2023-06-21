9ff8be8de916b0f9e6e69b3ab682c156c4e5fb9a
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-20 23:29:20 -0300

    automatizando o código para entrar nas plataformas

--- js/app.js
+++ js/app.js
@@ -11,21 +11,20 @@ window.addEventListener("scroll", () => {
 })
 
 // plataformas:
-
 const caixas = document.querySelectorAll('[data-caixa]');
 
 function ativarCaixa() {
-    const dataBoxHit = this.dataset.caixa;
+    const dataBoxHit = this.dataset.caixa; //MARCADO
     caixas.forEach(el => {
         el.classList.contains('active') ? el.classList.replace('active', 'disabled') : el.classList.add('disabled');
         if(dataBoxHit != el.dataset.caixa) return;
         el.classList.contains('disabled') ? el.classList.replace('disabled', 'active') : el.classList.add('active');
     })
 }
+caixas.forEach(box => box.addEventListener('click', ativarCaixa)); // voce pode colocar o 'MARCADO' como argumento-
+// aqui e chamar depois dentro da função para deixar ela mais renérica e reutilizavel
 
-caixas.forEach(box => box.addEventListener('click', ativarCaixa)) // TROCA POR THIS
-
-
+// Interação com fotos da Review:
 const containerPerfil = document.querySelectorAll('.perfil');
 const UserPerfil = document.querySelectorAll('.user');
 
