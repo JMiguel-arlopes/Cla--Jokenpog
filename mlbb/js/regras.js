a9e6fa946d543d68512b0ab2aacef788bb0b9158
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-20 23:33:01 -0300

    alterando a lógica da automação

--- mlbb/js/regras.js
+++ mlbb/js/regras.js
@@ -1,23 +1,20 @@
-const btnsRegra = document.querySelectorAll('.title-regra');
+const btnsRegra = document.querySelectorAll('.item-regra');
 const listaRegras = document.querySelectorAll('.lista-regras'); 
-const imgsRegra = document.querySelectorAll('.img-regra'); 
+const imgsRegra = document.querySelectorAll('.img-regra');
+const titulosRegra = document.querySelectorAll('.title-regra');
 
-function ativarRegras() {
-    const dataBtnRegra = this.dataset.regra;
+function ativarRegras(alvo, listaArray) {
+    const dataBtnRegra = alvo.dataset.regra;
     
-    listaRegras.forEach(regra => {
+    listaArray.forEach(regra => {
         const dataRegra = regra.dataset.regra;
         dataBtnRegra == dataRegra ? regra.classList.add('active') : regra.classList.remove('active');
     })
 
-    imgsRegra.forEach(imgRegra => {
-        const dataImg = imgRegra.dataset.regra;
-        dataBtnRegra == dataImg ? imgRegra.classList.add('active') : imgRegra.classList.remove('active');
-    })
-
-
     btnsRegra.forEach(btn => btn.classList.remove('active'))
-    this.classList.add('active')
+    alvo.classList.add('active')
 }
 
-btnsRegra.forEach(btn => btn.addEventListener('click', ativarRegras))
+btnsRegra.forEach(btn => btn.addEventListener('click', (e) => ativarRegras(e.target, listaRegras)));
+btnsRegra.forEach(btn => btn.addEventListener('click', (e) => ativarRegras(e.target, imgsRegra)));
+btnsRegra.forEach(btn => btn.addEventListener('click', (e) => ativarRegras(e.target, titulosRegra)));
