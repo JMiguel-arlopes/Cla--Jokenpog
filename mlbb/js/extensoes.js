2a13951bcd783dd29d42d848ab5bbb387dfe4443
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-20 23:31:59 -0300

    automatizando código das extensões do site mlbb

--- mlbb/js/extensoes.js
+++ mlbb/js/extensoes.js
@@ -1,18 +1,31 @@
 const containerBoxs = document.querySelector('.box-extensoes');
-const caixas = document.querySelectorAll('[data-box]');
+const caixas = document.querySelectorAll('[data-extensao]');
 
-function ativarCaixa() {
-    const dataElemento = this.dataset.box;
+// function ativarCaixa() {
+//     const dataElemento = this.dataset.box;
 
-    caixas.forEach(i => {
-        i.classList.remove('disabled');
+//     caixas.forEach(i => {
+//         i.classList.remove('disabled');
 
-        if (i == this) return;
-        const noClickBox = i.dataset.box;
-        containerBoxs.classList.remove(`active-box-${noClickBox}`);
-        i.classList.add('disabled');
+//         if (i == this) return;
+//         const noClickBox = i.dataset.box;
+//         containerBoxs.classList.remove(`active-box-${noClickBox}`);
+//         i.classList.add('disabled');
+//     })
+//     containerBoxs.classList.add(`active-box-${dataElemento}`);
+// }
+
+// caixas.forEach(elemento => elemento.addEventListener('click', ativarCaixa))
+
+
+
+
+function ativarExtensao(alvo, listaArray) {
+    const clickData = alvo.dataset.extensao;
+    listaArray.forEach(el => {
+        if(el.dataset.extensao != clickData) return;
+        el.classList.toggle('active')
     })
-    containerBoxs.classList.add(`active-box-${dataElemento}`);
 }
 
-caixas.forEach(elemento => elemento.addEventListener('click', ativarCaixa))
+caixas.forEach(box => box.addEventListener('click', (el) => ativarExtensao(el.target, caixas)))
