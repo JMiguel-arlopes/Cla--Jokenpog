501ec21c05acd419219cf6c1ee8046391b27e14a
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-23 23:17:32 -0300

    deletando arquivo label

--- mlbb/js/label-slide.js
+++
@@ -1,9 +0,0 @@
-const listaLabel = document.querySelectorAll('[data-label]');
-
-function ativarLabel(alvo, listaArray) {
-    listaArray.forEach(btn => btn.classList.remove('active')) //this.parentNode.querySelectorAll(var).forEach(...)
-    alvo.classList.add('active')
-    console.log('kk')
-}
-
-listaLabel.forEach(label => label.addEventListener('click', (e) => ativarLabel(e.target, listaLabel)))
