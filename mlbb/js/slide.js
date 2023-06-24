be911d2d56460e4b5931c4bf6d31ef7ec81a49cf
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-23 23:15:17 -0300

    automatizando melhor a sintaxe e inserindo interação com label

--- mlbb/js/slide.js
+++ mlbb/js/slide.js
@@ -1,22 +1,29 @@
 const inputs = document.querySelectorAll('[data-input]');
+const listaLabel = document.querySelectorAll('[data-label]');
+
 let counter = 1;
-function slide() {
-    
-    inputs.forEach(btnInput => {
-        btnInput.addEventListener('click', el => {
-            const clickInput = el.target.dataset.input;
-            counter = clickInput;
-        })
-    })
 
-    if (counter > inputs.length) {
-        counter = 1;
-    }
-
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-
-    setTimeout(slide, 8000)
+function atualizaLabel() {
+    listaLabel.forEach(el => el.classList.remove('active'));
+    document.querySelector(`[data-label="${counter}"]`).classList.add('active');
 }
+
+function slide() {
+
+    counter > inputs.length ? counter = 1 : false;
+    document.getElementById('radio' + counter).checked = true;
+    document.getElementById('radio' + counter).checked == true ? atualizaLabel() : false;
+
+    counter++
+    setTimeout(slide, 6000);
+}
+
+inputs.forEach(btnInput => {
+    btnInput.addEventListener('click', el => {
+        const clickInput = el.target.dataset.input;
+        counter = clickInput;
+        atualizaLabel();
+    })
+})
 
 slide();
