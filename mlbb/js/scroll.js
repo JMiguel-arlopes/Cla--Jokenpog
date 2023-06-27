deb249dd4c11843aedac930d1b94a081064547f9
JM-arlopes <jmiguelarlopes@gmail.com>
2023-06-26 23:52:57 -0300

    alterando tempo do slide

--- mlbb/js/scroll.js
+++ mlbb/js/scroll.js
@@ -1,7 +1,7 @@
 const animes = document.querySelectorAll('[data-anime]');
 
 function animation() {
-    const distanciaTopo = window.scrollY + (window.innerHeight * 5) / 6;
+    const distanciaTopo = window.scrollY + (window.innerHeight * 3) / 4;
 
     animes.forEach(el => {
         distanciaTopo > el.offsetTop ? el.classList.add('animate') : el.classList.remove('animate')
