be82a043df5ed2b88c4a6f25c816731dec9b2e27
JM-arlopes <jmiguelarlopes@gmail.com>
2023-07-04 21:15:18 -0300

    inserindo interação com botões back e next, melhorando sintaxe do código slide

--- mlbb/js/slide.js
+++ mlbb/js/slide.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll('[data-input]');
 const listaLabel = document.querySelectorAll('[data-label]');
+const arrowSlide = document.querySelectorAll('.arrow-slide');
 
 let counter = 1;
 
@@ -8,22 +9,41 @@ function atualizaLabel() {
     document.querySelector(`[data-label="${counter}"]`).classList.add('active');
 }
 
-function slide() {
-    
+function atualizaSlide() {
     counter > inputs.length ? counter = 1 : false;
     document.getElementById('radio' + counter).checked = true;
     document.getElementById('radio' + counter).checked == true ? atualizaLabel() : false;
-
-    counter++
-    setTimeout(slide, 6000);
 }
+
+function slide() {
+    atualizaSlide()
+    counter++
+    if(atualizaLabel) {
+        setTimeout(slide, 8000)
+    }
+}
+
+function backSlide() {
+    counter == 1 ? counter = inputs.length : counter--;
+    atualizaSlide();
+}
+
+function nextSlide() {
+    if(this.id == 'arrow-next') {
+        counter++;
+        atualizaSlide()
+    } else {
+        backSlide()
+    }   
+}
+
+slide();
+arrowSlide.forEach(arrow => arrow.addEventListener('click', nextSlide)); 
 
 inputs.forEach(btnInput => {
     btnInput.addEventListener('click', el => {
         const clickInput = el.target.dataset.input;
         counter = clickInput;
-        atualizaLabel();
+        atualizaSlide()
     })
 })
-
-slide();
